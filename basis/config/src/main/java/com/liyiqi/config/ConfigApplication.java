3104ab113e670a9bc8fb1e68bdd8ae0e71c4e1ca
lyq-97 <liyiqi97@163.com>
2020-05-30 22:59:04 +0800

    update

--- basis/config/src/main/java/com/liyiqi/config/ConfigApplication.java
+++ basis/config/src/main/java/com/liyiqi/config/ConfigApplication.java
@@ -2,8 +2,10 @@ package com.liyiqi.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ConfigApplication {
 
     public static void main(String[] args) {
